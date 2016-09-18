d6953fe82e78e34fe3a4185be5848944de446fbe
pauldebellis <pvdebellis@gmail.com>
2016-09-17 19:55:38 -0700

    adds get one and get all routes

--- api.js
+++ api.js
@@ -1,7 +1,18 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var apiRouter = express.Router();
+var Photo = require('./models/koan.js');
 
+apiRouter.get('/koans', function(req,res){
+    Koan.find({}, function(err, koans){
+        res.json(koans);
+    });
+});
 
+apiRouter.get('/koans/:id', function(req,res){
+    Koan.findOne({_id: req.params.id},function(err,photo){
+        res.json(photo);
+    })
+})
 
 module.exports=apiRouter;
