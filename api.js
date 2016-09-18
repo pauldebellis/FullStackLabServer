42fb062bdc47ea88393be4498563f2aa054e24eb
pauldebellis <pvdebellis@gmail.com>
2016-09-17 19:49:25 -0700

    successfully connects to DB

---
+++ api.js
@@ -0,0 +1,7 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var apiRouter = express.Router();
+
+
+
+module.exports=apiRouter;
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var dotEnv = require
+var dotEnv = require('dotenv').load({silent:true});
+var apiRoutes = require('./api.js');
+var mongoose = require('mongoose');
+var app = express();
+var port = process.env.PORT||3000;
+
+mongoose.connect(process.env.MONGODB_URL, function(err){
+    if(err){console.log("Oops, error connecting to DB");}
+    else{console.log("Connected to DB")}
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use("/api", apiRoutes);
+app.listen(port, function(){});
